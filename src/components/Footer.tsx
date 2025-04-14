a20f653809038038c043f85620071086343233c4
xiaodai <xiaodai@gmail.com>
2025-04-14 08:23:56 +0800

    Add Footer component with language support and responsive layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -146,7 +146,7 @@ const Footer = () => {
                 <svg className="w-5 h-5 text-primary mt-0.5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                 </svg>
-                <span className="text-gray-400">info@kmri.com</span>
+                <span className="text-gray-400">hr@kazamimri.com</span>
               </li>
               <li className="mt-6">
                 <Link 
